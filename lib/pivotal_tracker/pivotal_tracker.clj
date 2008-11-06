5eae8ace30bf8fcd20b2eafdc17cb17c06e38d40
Wilkes Joiner <wilkesjoiner@gmail.com>
2008-11-06 17:18:20 -0600

    fixed xml/emit to not print new lines

--- lib/pivotal_tracker/pivotal_tracker.clj
+++ lib/pivotal_tracker/pivotal_tracker.clj
@@ -6,6 +6,28 @@
 	   (org.apache.http.impl.client DefaultHttpClient)
 	   (org.apache.http.util EntityUtils)))
 
+;; stolen from clojure.xml but replace any println's with print's
+(defn emit-element [e]
+  (if (instance? String e)
+    (print e)
+    (do
+      (print (str "<" (name (:tag e))))
+      (when (:attrs e)
+	(doseq attr (:attrs e)
+	  (print (str " " (name (key attr)) "='" (val attr)"'"))))
+      (if (:content e)
+	(do
+	  (print ">")
+	  (doseq c (:content e)
+	    (emit-element c))
+	  (print (str "</" (name (:tag e)) ">")))
+	(print "/>")))))
+
+(defn emit [x]
+  (println "<?xml version='1.0' encoding='UTF-8'?>")
+  (emit-element x))
+
+
 (def *base-url* "http://www.pivotaltracker.com/services/v1/projects/")
 
 (defn- project-url [project-id]
@@ -47,7 +69,7 @@
     (reduce f [] hmap)))
 
 (defn struct-to-xml [name hmap]
-  (xml/emit {:tag name :content (xmlify hmap)}))
+  (emit {:tag name :content (xmlify hmap)}))
 
 ;; Get Project
 (defn get-project [token project-id]
