fcd50e3c9b277f4b8c001aab3450d379aeb57e2b
Wilkes Joiner <wilkesjoiner@gmail.com>
2008-11-07 13:20:21 -0600

    extraced httpclient stuff into webclient

--- lib/pivotal_tracker/pivotal_tracker.clj
+++ lib/pivotal_tracker/pivotal_tracker.clj
@@ -1,11 +1,7 @@
 (ns pivotal-tracker
   (:require (clojure.contrib (except :as except))
-	    (pivotal-tracker (xml :as xml)))
-  (:import (org.apache.http.client HttpClient)
-	   (org.apache.http.client.methods HttpGet HttpPost HttpDelete HttpPut)
-	   (org.apache.http.impl.client DefaultHttpClient)
-	   (org.apache.http.entity StringEntity)
-	   (org.apache.http.util EntityUtils)))
+	    (pivotal-tracker (xml :as xml)
+			     (webclient :as client))))
 
 ; URL creation
 (def *base-url* "http://www.pivotaltracker.com/services/v1/projects/")
@@ -19,36 +15,12 @@
   ([project-id story-id]
      (str (story-url project-id) "/" story-id)))
 
-(defn build-request
-  ([token request]
-     (doto request
-       (addHeader "Token" token)
-       (addHeader "Content-type" "application/xml")))
-  ([token request data]
-     (doto (build-request token request)
-       (setEntity (StringEntity. data)))))
+(defn- fetch-item [token url]
+  (xml/to-struct (-> (client/fetch-as-xml token url) :content first)))
 
-(defn do-request [request]
-  (.execute (DefaultHttpClient.) request))
-
-(defn fetch-url [token url]
-  (do-request (build-request token (HttpGet. url))))
-
-(defn post-url [token url data]
-  (do-request (build-request token (HttpPost. url) data)))
-
-(defn response-to-string [response]
-  (EntityUtils/toString (.getEntity response)))
-
-(defn fetch-as-xml [token url]
-  (-> (fetch-url token url) response-to-string xml/parse))
-
-(defn fetch-item [token url]
-  (xml/to-struct (-> (fetch-as-xml token url) :content first)))
-
-(defn fetch-collection [token url]
+(defn- fetch-collection [token url]
   (reduce #(cons (xml/to-struct %2) %1) 
-	  [] (-> (fetch-as-xml token url) :content second :content)))
+	  [] (-> (client/fetch-as-xml token url) :content second :content)))
 
 ;; Get Project
 (defn get-project [token project-id]
@@ -79,24 +51,23 @@
 
 (defn add-story [token project-id story]
   (except/throw-if (not (valid-story? story)) "Invalid story")
-  (post-url token
+  (client/post token
 	    (story-url project-id)
 	    (xml/to-xml :story story)))
 
 ;; Updating Stories
 ;; Always getting a 500?!?!
 (defn update-story [token project-id story-id update]
-  (post-url token
-	    (story-url project-id story-id)
-	    :story update))
+  (client/post token
+	       (story-url project-id story-id)
+	       :story update))
 
 ;; Deleting Stories
 (defn delete-story [token project-id story-id]
-  (do-request (build-request token (HttpDelete. (story-url project-id story-id)))))
+  (client/delete token (story-url project-id story-id)))
 
 ;; Deliver All Finished Stories
 (defn deliver-finished-stories [token project-id]
-  (do-request
-   (build-request token (HttpPut. (str (project-url project-id) 
-				       "/stories_deliver_all_finished")))))
+  (client/put token 
+	      (str (project-url project-id) "/stories_deliver_all_finished")))
 
---
+++ lib/pivotal_tracker/webclient/webclient.clj
@@ -0,0 +1,39 @@
+(ns pivotal-tracker.webclient
+  (:refer-clojure :exclude (get))
+  (:require (clojure.contrib (except :as except))
+	    (pivotal-tracker (xml :as xml)))
+  (:import (org.apache.http.client HttpClient)
+	   (org.apache.http.client.methods HttpGet HttpPost HttpDelete HttpPut)
+	   (org.apache.http.impl.client DefaultHttpClient)
+	   (org.apache.http.entity StringEntity)
+	   (org.apache.http.util EntityUtils)))
+
+(defn build-request
+  ([token request]
+     (doto request
+       (addHeader "Token" token)
+       (addHeader "Content-type" "application/xml")))
+  ([token request data]
+     (doto (build-request token request)
+       (setEntity (StringEntity. data)))))
+
+(defn do-request [token request]
+  (.execute (DefaultHttpClient.) (build-request token request)))
+
+(defn get [token url]
+  (do-request token (HttpGet. url)))
+
+(defn post [token url data]
+  (do-request token (HttpPost. url) data))
+
+(defn delete [token url]
+  (do-request token (HttpDelete. url)))
+
+(defn put [token url]
+  (do-request token (HttpPut. url)))
+
+(defn response-to-string [response]
+  (EntityUtils/toString (.getEntity response)))
+
+(defn fetch-as-xml [token url]
+  (-> (get token url) response-to-string xml/parse))
