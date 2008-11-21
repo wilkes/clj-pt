492584985559065a2aadcf1bf7266ef3572b5ec4
Wilkes Joiner <wilkesjoiner@gmail.com>
2008-11-21 12:44:00 -0600

    create an emacs script to run the slime env

---
+++ emacs/pt-slime.el
@@ -0,0 +1,20 @@
+(require 'slime)
+(require 'clojure-auto)
+(require 'clojure-paredit)
+
+(add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
+
+;; These are extra key defines because I kept typing them.  
+;; Within clojure-mode, have Ctrl-x Ctrl-e evaluate the last 
+;; expression.
+;; Ctrl-c Ctrl-e is also there, because I kept typoing it.
+(add-hook 'clojure-mode-hook
+          '(lambda ()
+             (define-key clojure-mode-map "\C-c\C-e" 'lisp-eval-last-sexp)
+             (define-key clojure-mode-map "\C-x\C-e" 'lisp-eval-last-sexp)))
+
+
+(setq swank-clojure-jar-path (expand-file-name "jar/clojure.jar"))
+(setq swank-clojure-extra-classpaths (directory-files "jars" t "jar$"))
+(require 'swank-clojure-autoload)
+(slime)
