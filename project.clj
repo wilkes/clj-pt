92f5ca755db2024f42a84c160db9e40faa74fbaa
wilkes <wilkesjoiner@gmail.com>
2009-12-04 08:15:08 -0600

    start 0.2.0, resources/ not needed

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-pt "0.1.1"
+(defproject clj-pt "0.2.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
                  [org.apache.httpcomponents/httpclient "4.0"]]
