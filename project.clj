4ab0896e8e00802e10db6b92bf034b899a824594
wilkes <wilkesjoiner@gmail.com>
2009-12-17 14:01:31 -0600

    version 0.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-pt "0.3.0-SNAPSHOT"
+(defproject clj-pt "0.3.0"
   :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
                  [org.apache.httpcomponents/httpclient "4.0"]
